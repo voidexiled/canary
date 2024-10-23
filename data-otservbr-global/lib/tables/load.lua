281bac0ad58752c8e0cacd46b47f63d053d99d0a
voidexiled <l20070518@cdmadero.tecnm.mx>
2024-10-23 12:48:26 -0600

    fix: exercise_training_weapons fixed

--- data-otservbr-global/lib/tables/load.lua
+++ data-otservbr-global/lib/tables/load.lua
@@ -1,3 +1,2 @@
 dofile(DATA_DIRECTORY .. "/lib/tables/teleport_item_destinations.lua")
 dofile(DATA_DIRECTORY .. "/lib/tables/town.lua")
-dofile(DATA_DIRECTORY .. "/lib/tables/exercise_training.lua")
--- data/scripts/actions/items/exercise_training_weapons.lua
+++ data/scripts/actions/items/exercise_training_weapons.lua
@@ -116,10 +116,14 @@ local function exerciseTrainingEvent(playerId, tilePosition, weaponId, dummyId)
 
 	if weapon:getAttribute(ITEM_ATTRIBUTE_CHARGES) <= 0 then
 		weapon:remove(1)
-		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "Your training weapon has disappeared.")
-		leaveExerciseTraining(playerId)
-		return false
+		local weapon = player:getItemById(weaponId, true)
+		if not weapon or (not weapon:isItem() or not weapon:hasAttribute(ITEM_ATTRIBUTE_CHARGES)) then
+			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "Your training weapon has disappeared.")
+			leaveExerciseTraining(playerId)
+			return false
+		end
 	end
+
 
 	local vocation = player:getVocation()
 	_G.OnExerciseTraining[playerId].event = addEvent(exerciseTrainingEvent, vocation:getBaseAttackSpeed() / configManager.getFloat(configKeys.RATE_EXERCISE_TRAINING_SPEED), playerId, tilePosition, weaponId, dummyId)

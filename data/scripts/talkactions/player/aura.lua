a8ca96d3a85ab62ed0370224c3707e549f656b6c
voidexiled <l20070518@cdmadero.tecnm.mx>
2024-10-23 16:10:57 -0600

    refactor: aura effect modified

--- data/scripts/talkactions/player/aura.lua
+++ data/scripts/talkactions/player/aura.lua
@@ -1,58 +1,82 @@
 local config_aura = {
-    effect = 67; -- efeito que vai ficar girando no player
-    effect_health = 162; -- efeito qnd curar o player
+    effect = 260, -- efeito que vai ficar girando no player
+    effect_health = 162, -- efeito qnd curar o player
     level_use = { -- level que vai ser usado (min/max)
         min = 1,
-        max = 600,
-    } ;
-    health = 10; -- tanto que vai curar por time configurado
+        max = 600
+    },
+    health = 10, -- tanto que vai curar por time configurado
     pos_aura = { -- não mexa
-        [1] = {x = 0, y = -1};
-        [2] = {x = 1, y = -1};
-        [3] = {x = 1, y = 0};
-        [4] = {x = 1, y = 1};
-        [5] = {x = 0, y = 1};
-        [6] = {x = -1, y = 1};
-        [7] = {x = -1, y = 0};
-        [8] = {x = -1, y = -1};
-    };
-    storage_pos = 1547637649; -- não mexa e nem repita esse valor em outro script!!
-    storage = 165477963; -- não mexa e nem repita esse valor em outro script!!
-    tempo_aura = 2000; -- tempo da aurea para passar em cada posição
+        [1] = {
+            x = 0,
+            y = -1
+        },
+        [2] = {
+            x = 1,
+            y = -1
+        },
+        [3] = {
+            x = 1,
+            y = 0
+        },
+        [4] = {
+            x = 1,
+            y = 1
+        },
+        [5] = {
+            x = 0,
+            y = 1
+        },
+        [6] = {
+            x = -1,
+            y = 1
+        },
+        [7] = {
+            x = -1,
+            y = 0
+        },
+        [8] = {
+            x = -1,
+            y = -1
+        }
+    },
+    storage_pos = 1547637649, -- não mexa e nem repita esse valor em outro script!!
+    storage = 165477963, -- não mexa e nem repita esse valor em outro script!!
+    tempo_aura = 2000 -- tempo da aurea para passar em cada posição
 }
 
 local aura_table = {
     [1] = {
-        effect = CONST_ME_MAGIC_BLUE,
+        effect = CONST_ME_MAGIC_BLUE
     }
 }
 
 local function calculePosAurea(player_uid)
     local player = Player(player_uid)
-    if(not(player))then
-        return(nil)
+    if (not (player)) then
+        return (nil)
     end
 
-    if(player:getStorageValue(config_aura.storage) <= 0)then
-        return(nil)
+    if (player:getStorageValue(config_aura.storage) <= 0) then
+        return (nil)
     end
 
-    if(player:getStorageValue(config_aura.storage_pos) >= 8)then
+    if (player:getStorageValue(config_aura.storage_pos) >= 8) then
         player:setStorageValue(config_aura.storage_pos, 0)
     end
 
     player:setStorageValue(config_aura.storage_pos, player:getStorageValue(config_aura.storage_pos) + 1)
-    return(player:getStorageValue(config_aura.storage_pos))
+    return (player:getStorageValue(config_aura.storage_pos))
 end
 
 local function posAurea(player_uid)
     local player = Player(player_uid)
-    if(not(player))then
-        return(nil)
+    if (not (player)) then
+        return (nil)
     end
 
-    if(player:getStorageValue(config_aura.storage) <= 0)then
-        return(nil)
+    if (player:getStorageValue(config_aura.storage) <= 0) then
+        return (nil)
     end
 
     local pos_x = player:getPosition().x
@@ -61,24 +85,24 @@ local function posAurea(player_uid)
 
     local aa = calculePosAurea(player_uid)
     local pos = Position(pos_x, pos_y, pos_z)
-    if(not(pos))then
-        return(nil)
+    if (not (pos)) then
+        return (nil)
     end
-   return(pos)
+    return (pos)
 end
 
 local function aurea(player_uid)
     local player = Player(player_uid)
-    if(not(player))then
-        return(nil)
+    if (not (player)) then
+        return (nil)
     end
 
-    if(player:getStorageValue(config_aura.storage) <= 0)then
-        return(nil)
+    if (player:getStorageValue(config_aura.storage) <= 0) then
+        return (nil)
     end
 
     -- player:addHealth(config_aura.health)
-    --player:getPosition():sendMagicEffect(config_aura.effect_health)
+    -- player:getPosition():sendMagicEffect(config_aura.effect_health)
 
     local position = posAurea(player_uid)
     position:sendMagicEffect(config_aura.effect)
@@ -89,16 +113,19 @@ local aura = TalkAction("/aura")
 function aura.onSay(player, words, param)
     local player_level = player:getLevel()
     if param == "on" then
-        if(player:getLevel() > config_aura.level_use.max)then
-            return(not(player:sendTextMessage(MESSAGE_STATUS_SMALL, player:getName() .. " seu level(" .. player:getLevel() .. ") precisa ser maior(" .. config_aura.level_use.max)))
+        if (player:getLevel() > config_aura.level_use.max) then
+            return (not (player:sendTextMessage(MESSAGE_STATUS_SMALL, player:getName() .. " seu level(" ..
+                player:getLevel() .. ") precisa ser maior(" .. config_aura.level_use.max)))
         end
 
-        if(player:getLevel() < config_aura.level_use.min)then
-            return(not(player:sendTextMessage(MESSAGE_STATUS_SMALL, player:getName() .. " seu level(" .. player:getLevel() .. ") precisa ser menor(" .. config_aura.level_use.max)))
+        if (player:getLevel() < config_aura.level_use.min) then
+            return (not (player:sendTextMessage(MESSAGE_STATUS_SMALL, player:getName() .. " seu level(" ..
+                player:getLevel() .. ") precisa ser menor(" .. config_aura.level_use.max)))
         end
 
-        if(player:getStorageValue(config_aura.storage) >= 1)then
-            return(not(player:sendTextMessage(MESSAGE_STATUS_SMALL, player:getName() .. " Você ja esta com a aurea ligada")))
+        if (player:getStorageValue(config_aura.storage) >= 1) then
+            return (not (player:sendTextMessage(MESSAGE_STATUS_SMALL,
+                player:getName() .. " Você ja esta com a aurea ligada")))
         end
 
         player:setStorageValue(config_aura.storage, 1)
@@ -108,7 +135,7 @@ function aura.onSay(player, words, param)
         player:setStorageValue(config_aura.storage, 0)
     end
 
-  return(false)
+    return (false)
 end
 
 aura:separator(" ")

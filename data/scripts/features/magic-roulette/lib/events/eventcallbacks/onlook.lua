d77f9d54c437c5a41fc5adc4f21fb321cff1c76f
voidexiled <l20070518@cdmadero.tecnm.mx>
2024-10-24 05:55:52 -0600

    fix: onlook fix

--- data/scripts/features/magic-roulette/lib/events/eventcallbacks/onlook.lua
+++ data/scripts/features/magic-roulette/lib/events/eventcallbacks/onlook.lua
@@ -1,7 +1,7 @@
 local Constants = require('data/scripts/features/magic-roulette/lib/core/constants')
 
 local ec = EventCallback("PlayerOnLookRouletteDummy")
-ec.onLook = function(self, thing, position, distance, description)
+function ec.onLook(self, thing, position, distance, description)
     if thing:getName() == Constants.ROULETTE_DUMMY_NAME then
         local item = ItemType(thing:getOutfit().lookTypeEx)
 

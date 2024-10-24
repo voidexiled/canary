e3db2a1da799cfc2f5ad2d10acdabc25847c317a
voidexiled <l20070518@cdmadero.tecnm.mx>
2024-10-24 04:10:38 -0600

    feat(server): change_gold supports nugget coin

--- data/scripts/actions/items/change_gold.lua
+++ data/scripts/actions/items/change_gold.lua
@@ -1,24 +1,35 @@
 local config = {
-	[ITEM_GOLD_COIN] = { changeTo = ITEM_PLATINUM_COIN },
-	[ITEM_PLATINUM_COIN] = { changeBack = ITEM_GOLD_COIN, changeTo = ITEM_CRYSTAL_COIN },
-	[ITEM_CRYSTAL_COIN] = { changeBack = ITEM_PLATINUM_COIN },
+    [ITEM_GOLD_COIN] = {
+        changeTo = ITEM_PLATINUM_COIN
+    },
+    [ITEM_PLATINUM_COIN] = {
+        changeBack = ITEM_GOLD_COIN,
+        changeTo = ITEM_CRYSTAL_COIN
+    },
+    [ITEM_CRYSTAL_COIN] = {
+        changeBack = ITEM_PLATINUM_COIN,
+        changeTo = ITEM_NUGGET_COIN
+    },
+    [ITEM_NUGGET_COIN] = {
+        changeBack = ITEM_CRYSTAL_COIN
+    }
 }
 
 local changeGold = Action()
 
 function changeGold.onUse(player, item, fromPosition, target, toPosition, isHotkey)
-	local coin = config[item:getId()]
-	if coin.changeTo and item.type == 100 then
-		item:remove()
-		player:addItem(coin.changeTo, 1)
-		return true
-	elseif coin.changeBack then
-		item:remove(1)
-		player:addItem(coin.changeBack, 100)
-		return true
-	end
-	return false
+    local coin = config[item:getId()]
+    if coin.changeTo and item.type == 100 then
+        item:remove()
+        player:addItem(coin.changeTo, 1)
+        return true
+    elseif coin.changeBack then
+        item:remove(1)
+        player:addItem(coin.changeBack, 100)
+        return true
+    end
+    return false
 end
 
-changeGold:id(ITEM_GOLD_COIN, ITEM_PLATINUM_COIN, ITEM_CRYSTAL_COIN)
+changeGold:id(ITEM_GOLD_COIN, ITEM_PLATINUM_COIN, ITEM_CRYSTAL_COIN, ITEM_NUGGET_COIN)
 changeGold:register()

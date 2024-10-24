779118df0d4da79b314122fd43e1397e3c57b808
voidexiled <l20070518@cdmadero.tecnm.mx>
2024-10-24 05:53:12 -0600

    fix: onlook fix

--- data/scripts/features/magic-roulette/lib/events/eventcallbacks/onlook.lua
+++ data/scripts/features/magic-roulette/lib/events/eventcallbacks/onlook.lua
@@ -1,21 +1,12 @@
---[[
-	Description: This file is part of Roulette System (refactored)
-	Author: Lyµ
-	Discord: Lyµ#8767
-]]
-
 local Constants = require('data/scripts/features/magic-roulette/lib/core/constants')
 
-local ec = EventCallback
+local ec = EventCallback("PlayerOnLookRouletteDummy")
 ec.onLook = function(self, thing, position, distance, description)
-	if thing:getName() == Constants.ROULETTE_DUMMY_NAME then
-		local item = ItemType(thing:getOutfit().lookTypeEx)
+    if thing:getName() == Constants.ROULETTE_DUMMY_NAME then
+        local item = ItemType(thing:getOutfit().lookTypeEx)
 
-		return ('You see %s.\n%s'):format(
-			item:getName(),
-			item:getDescription()
-		)
-	end
-	return description
+        return ('You see %s.\n%s'):format(item:getName(), item:getDescription())
+    end
+    return description
 end
 ec:register(1)

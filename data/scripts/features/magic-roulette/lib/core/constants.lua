e2873d0f4e829d4e91c81c94b766f77e81908da3
voidexiled <l20070518@cdmadero.tecnm.mx>
2024-10-24 05:49:38 -0600

    feat: added magic roulette files

---
+++ data/scripts/features/magic-roulette/lib/core/constants.lua
@@ -0,0 +1,11 @@
+--[[
+	Description: This file is part of Roulette System (refactored)
+	Author: Ly�
+	Discord: Ly�#8767
+]] return {
+    ROULETTE_DUMMY_NAME = 'Roulette Dummy',
+
+    PLAY_STATUS_ROLLING = 0,
+    PLAY_STATUS_PENDING = 1,
+    PLAY_STATUS_DELIVERED = 2
+}

a0d9a5da90a91b1270725ab9e9475e5ae9cb1197
voidexiled <l20070518@cdmadero.tecnm.mx>
2024-10-25 16:10:30 -0600

    feat: rotten blood entrance and exit tile

---
+++ data-otservbr-global/scripts/movements/quests/rotten_blood/entrance.lua
@@ -0,0 +1,36 @@
+local config = {
+    [62000] = { -- entrance aid
+        to = Position(34070, 31975, 14),
+        storage = 61000,
+        minValue = 1,
+        failMessage = "You need to talk with Torkada to enter."
+    }
+
+}
+
+local rottenBloodEntranceMoveEvent = MoveEvent()
+
+function rottenBloodEntranceMoveEvent.onStepIn(creature, item, position, fromPosition)
+    local player = creature:getPlayer()
+    if not player then
+        return false
+    end
+
+    local missionTo = config[item.uid].to
+    if not missionTo then
+        return false
+    end
+
+    if config[item.uid].storage and player:getStorageValue(config[item.uid].storage) < config[item.uid].minValue then
+        player:sendTextMessage(MESSAGE_EVENT_ADVANCE, config[item.uid].failMessage)
+        player:teleportTo(fromPosition)
+        return false
+    end
+
+    player:teleportTo(missionTo.to)
+    return true
+end
+
+rottenBloodEntranceMoveEvent:type("stepin")
+rottenBloodEntranceMoveEvent:aid(61000)
+rottenBloodEntranceMoveEvent:register()
---
+++ data-otservbr-global/scripts/movements/quests/rotten_blood/exits.lua
@@ -0,0 +1,26 @@
+local config = {
+    [63000] = { -- exit entrance
+        to = Position(32954, 32398, 9)
+    }
+}
+
+local rottenBloodEntranceExitMoveEvent = MoveEvent()
+
+function rottenBloodEntranceExitMoveEvent.onStepIn(creature, item, position, fromPosition)
+    local player = creature:getPlayer()
+    if not player then
+        return false
+    end
+
+    local missionTo = config[item.uid].to
+    if not missionTo then
+        return false
+    end
+
+    player:teleportTo(missionTo.to)
+    return true
+end
+
+rottenBloodEntranceExitMoveEvent:type("stepin")
+rottenBloodEntranceExitMoveEvent:aid(63000)
+rottenBloodEntranceExitMoveEvent:register()

6594b93d69a5e1004a66e754268d4145e4eae075
voidexiled <l20070518@cdmadero.tecnm.mx>
2024-11-11 21:50:05 -0600

    feat: !buypromotion

---
+++ data/scripts/talkactions/player/buypromotion.lua
@@ -0,0 +1,20 @@
+local buypromotion = TalkAction("!promotion")
+
+function buypromotion.onSay(player, words, param)
+	local totalCost = 20000
+	local promotion = player:getVocation():getPromotion()
+	local hasPromotion = player:kv():get("promoted")
+	if not promotion or hasPromotion then
+		player:sendTextMessage(MESSAGE_STATUS, "You are already promoted!")
+	elseif not player:removeMoneyBank(totalCost) then
+		npcHandler:say("You do not have enough money!", npc, player)
+	else
+		player:sendTextMessage(MESSAGE_INFO_DESCR, "You have promoted!")
+		player:setVocation(promotion)
+		player:kv():set("promoted", true)
+	end
+	return true
+end
+
+buypromotion:groupType("normal")
+buypromotion:register()

687a3884bd2eb33aae760c03ff3339a92227ea8f
voidexiled <l20070518@cdmadero.tecnm.mx>
2024-11-12 23:23:24 -0600

    fix: remove skull message

--- data/scripts/actions/items/remove_skull.lua
+++ data/scripts/actions/items/remove_skull.lua
@@ -4,9 +4,9 @@ function removeSkull.onUse(player, item, fromPosition, target, toPosition, isHot
     if player:getSkull() ~= SKULL_NONE then
         item:remove(1)
         player:setSkull(SKULL_NONE)
-        player:sendTextMessage(MESSAGE_EVENT, "You have removed your skull.")
+        player:sendTextMessage(MESSAGE_STATUS, "You have removed your skull.")
     else
-        player:sendTextMessage(MESSAGE_EVENT, "You don't have a skull.")
+        player:sendTextMessage(MESSAGE_STATUS, "You don't have a skull.")
     end
     return true
 end

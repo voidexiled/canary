ce0a2e609c8950251a985cd6243f52659faa7857
voidexiled <l20070518@cdmadero.tecnm.mx>
2024-11-12 23:20:40 -0600

    fix: remover skull

--- data/scripts/actions/items/remove_skull.lua
+++ data/scripts/actions/items/remove_skull.lua
@@ -1,15 +1,15 @@
-local action = Action()
+local removeSkull = Action()
 
-function action.onUse(player, item, fromPosition, target, toPosition, isHotkey)
+function removeSkull.onUse(player, item, fromPosition, target, toPosition, isHotkey)
     if player:getSkull() ~= SKULL_NONE then
+        item:remove(1)
         player:setSkull(SKULL_NONE)
         player:sendTextMessage(MESSAGE_EVENT, "You have removed your skull.")
-        item:remove(1)
     else
         player:sendTextMessage(MESSAGE_EVENT, "You don't have a skull.")
     end
     return true
 end
 
-action:id(12904)
-action:register()
+removeSkull:id(12904)
+removeSkull:register()

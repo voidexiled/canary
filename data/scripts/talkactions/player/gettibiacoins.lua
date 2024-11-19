4dc3b9692a0baa20bffa08f4a2616e4e95b678d1
voidexiled <l20070518@cdmadero.tecnm.mx>
2024-11-18 22:52:33 -0600

    feat: freetc talkaction

---
+++ data/scripts/talkactions/player/gettibiacoins.lua
@@ -0,0 +1,11 @@
+local freetc = TalkAction("!freetc")
+
+function freetc.onSay(player, words, param)
+	player:addTransferableCoins(10000)
+	player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You have added 10000 tibia coins to your balance. Your total is now " .. player:getTransferableCoins() .. ".")
+	player:getPosition():sendMagicEffect(CONST_ME_ORCSHAMAN_FIRE)
+	return true
+end
+
+freetc:groupType("normal")
+freetc:register()

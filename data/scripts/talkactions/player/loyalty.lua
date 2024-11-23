bfd30089486464de0f2559d16e9802746d5a4060
voidexiled <l20070518@cdmadero.tecnm.mx>
2024-11-22 21:04:12 -0600

    feat: !loyalty command
    !loyalty
    player access

---
+++ data/scripts/talkactions/player/loyalty.lua
@@ -0,0 +1,19 @@
+local loyalty = TalkAction("!loyalty")
+
+function loyalty.onSay(player, words, param, type)
+    local loyaltyTitle = player:getLoyaltyTitle()
+    local loyaltyPoints = player:getLoyaltyPoints()
+    local loyaltyBonus = player:getLoyaltyBonus()
+
+    if loyaltyTitle ~= nil then
+        player:sendTextMessage(MESSAGE_EVENT_ADVANCE, string.format("You are a %s with %d loyalty points and %d bonus points.", loyaltyTitle, loyaltyPoints, loyaltyBonus))
+        return true
+    else
+        player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You don't have any loyalty.")
+    end
+
+    return true
+end
+
+loyalty:accountType("normal")
+loyalty:register()
